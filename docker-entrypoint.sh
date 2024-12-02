2ae1b0418ad8c9d6b5f0596b432c1174a43a17eb
Marco <marco@bon.so>
2024-12-02 12:23:01 +0100

    feat: customise entrypoint to support multiple providers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,15 +4,16 @@ set -eo pipefail
 SECONDS=0
 
 # required environment variables
-: "${AWS_ACCESS_KEY_ID:?Please set the environment variable.}"
-: "${AWS_REGION:?Please set the environment variable.}"
-: "${AWS_S3_ENDPOINT:?Please set the environment variable.}"
-: "${AWS_SECRET_ACCESS_KEY:?Please set the environment variable.}"
+: "${ACCESS_KEY_ID:?Please set the environment variable.}"
+: "${S3_REGION:?Please set the environment variable.}"
+: "${S3_ENDPOINT:?Please set the environment variable.}"
+: "${S3_BUCKET:?Please set the environment variable.}"
+: "${SECRET_ACCESS_KEY:?Please set the environment variable.}"
 : "${POSTGRES_DB:?Please set the environment variable.}"
 : "${POSTGRES_PASSWORD:?Please set the environment variable.}"
 
 # optional environment variables with defaults
-AWS_S3_STORAGE_CLASS="${AWS_S3_STORAGE_CLASS:-STANDARD_IA}"
+S3_PROVIDER="${S3_PROVIDER:-Other}"
 POSTGRES_HOST="${POSTGRES_HOST:-postgres}"
 POSTGRES_PORT="${POSTGRES_PORT:-5432}"
 POSTGRES_USER="${POSTGRES_USER:-postgres}"
@@ -43,7 +44,7 @@ echo "Uploading to S3..."
 rclone copyto \
   --s3-no-check-bucket \
   "./${BACKUP_FILE_NAME}" \
-  ":s3,access_key_id=${AWS_ACCESS_KEY_ID},provider=AWS,region=${AWS_REGION},secret_access_key=${AWS_SECRET_ACCESS_KEY},storage_class=${AWS_S3_STORAGE_CLASS}:${AWS_S3_ENDPOINT}/${BACKUP_FILE_NAME}"
+  ":s3,access_key_id=${ACCESS_KEY_ID},provider=${S3_PROVIDER},region=${S3_REGION},secret_access_key=${SECRET_ACCESS_KEY},endpoint=${S3_ENDPOINT}:${S3_BUCKET}/${BACKUP_FILE_NAME}"
 echo "Uploading to S3... Done."
 
 if [ -n "${WEBGAZER_HEARTBEAT_URL}" ]; then
